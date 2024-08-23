1aaae4a4f2be10b056990c45b20ee7296bc0bed5
yeobi <kimhyjjang@gmail.com>
2024-08-24 01:33:59 +0900

    fix: 좋아요 수정

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from .views import CommentList, CommentDetail, CommentLikeAdd
 urlpatterns = [
     path('<int:communityId>/', CommentList.as_view()),
     path('detail/<int:pk>/', CommentDetail.as_view()),
-    path('detail/<int:pk>/like', CommentLikeAdd.as_view()),
+    path('detail/<int:pk>/like/', CommentLikeAdd.as_view()),
 ]
