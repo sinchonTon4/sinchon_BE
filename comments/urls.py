3570cd95c5cf69e8cbf893ef4c120f7478b1bb8f
yeobi <kimhyjjang@gmail.com>
2024-08-23 23:18:37 +0900

    comments 작성

---
+++ comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CommentList, CommentDetail
+
+urlpatterns = [
+    path('', CommentList.as_view()),
+    path('<int:pk>/', CommentDetail.as_view()),
+    path('<int:communityId>/', CommentList.as_view()),
+]
