c12b6cbde93a0c287c6f11d2bf79c3079f21680d
soli-ziyo <solac@ewha.ac.kr>
2024-08-23 23:47:55 +0900

    merge 충돌 해결

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from auths.views import *
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('comments/', include('comments.urls')),
+]
