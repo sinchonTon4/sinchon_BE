f8236e6853fd5df58efc39771cd6044783892ba3
soli-ziyo <solac@ewha.ac.kr>
2024-08-24 04:40:00 +0900

    fix:api 수정

--- cobying/models.py
+++ cobying/models.py
@@ -10,7 +10,7 @@ class Cobying(models.Model):
     created_at = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=50)  # 제목
     description = models.TextField()  # 공구설명
-    img = models.ImageField(blank=True, upload_to='copying_images/')
+    img = models.ImageField(blank=True, upload_to='cobying_images/')
     tag = models.ForeignKey(HashTag, on_delete=models.CASCADE)  # 해시태그 공동구매
     price = models.IntegerField(blank=True)
     product_name = models.TextField(max_length=50)
@@ -21,7 +21,7 @@ class Cobying(models.Model):
         ('food', '식료품'),
         ('food', '생필품'),
     ]
-    product_category = models.CharField(choices=CATEGORY_CHOICES, blank=True, null=True)
+    product_category = models.CharField(choices=CATEGORY_CHOICES, blank=True, null=True, max_length=50)
 
     def __str__(self):
         return self.product_category
--- cobying/urls.py
+++ cobying/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CobyingCreateView, CobyingListView, CobyingDetail
 
 urlpatterns = [
-    path('', CobyingCreateView.as_view()),
+    path('create/', CobyingCreateView.as_view()),
     path('', CobyingListView.as_view()),
     path('<int:pk>/', CobyingDetail.as_view()),
 ]
