99585372fb66518aa1f0e065184892a86af28c95
yeobi <kimhyjjang@gmail.com>
2024-08-24 04:16:35 +0900

    cobying 구현

--- cobying/urls.py
+++ cobying/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CobyingCreateView
+from .views import CobyingCreateView, CobyingListView, CobyingDetail
 
 urlpatterns = [
-    path('create/', CobyingCreateView.as_view()), 
-
+    path('', CobyingCreateView.as_view()),
+    path('', CobyingListView.as_view()),
+    path('<int:pk>/', CobyingDetail.as_view()),
 ]
--- cobying/views.py
+++ cobying/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Cobying
 from .serializers import CobyingSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import get_object_or_404
 
 class CobyingCreateView(APIView):
     def post(self, request, *args, **kwargs):
@@ -19,3 +21,52 @@ class CobyingCreateView(APIView):
             # 데이터가 유효하지 않은 경우 오류 응답 반환
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class CobyingListView(APIView):
+    def get(self, request, *args, **kwargs):
+        product_category = request.query_params.get('category')
+        order = request.query_params.get('order')
+
+        queryset = Cobying.objects.all().order_by('-created_at')
+
+        if product_category:
+            queryset = queryset.filter(category__icontains=product_category)
+        if order == 'people_num':
+            queryset = queryset.order_by('-people_num')
+
+        paginator = PageNumberPagination()
+        paginator.page_size = 3
+        cobyings = paginator.paginate_queryset(queryset, request)
+        serializer = CobyingSerializer(cobyings, many=True)
+
+        serialized_data = [
+            {
+                "id": cobying['id'],
+                "title": cobying['title'],
+                "description": cobying['description'],
+                "img": cobying['img'],
+                "tag": cobying['tag'],
+                "price": cobying['price'],
+                "product_name": cobying['product_name'],
+                "link": cobying['link'],
+                "people_num": cobying['people_num'],
+                "product_category": cobying['product_category'],
+                "created_at": cobying['created_at'],
+            } for cobying in serializer.data
+        ]
+
+        return paginator.get_paginated_response({
+            "status": 200,
+            "message": "공동구매 조회 완료.",
+            "data": serialized_data
+        })
+
+
+class CobyingDetail(APIView):
+    def get_object(request, pk):
+        cobying = get_object_or_404(Cobying, pk=pk)
+        return cobying
+
+    def get(self, request, pk):
+        cobying = self.get_object(pk)
+        serializer = CobyingSerializer(cobying)
+        return Response(serializer.data)
