63587941af560d29c4bcc4a7ecdc98891a9ec439
soli-ziyo <solac@ewha.ac.kr>
2024-08-24 00:55:16 +0900

    fix:comment 조회 수정 완료

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    community_id = serializers.IntegerField(source='community.id', read_only=True)
+    
     class Meta:
         model = Comment
-        fields= ['user_id', 'community_id', 'description', 'like']
+        fields= ['id', 'user_id', 'community_id', 'description', 'like']
